353d45e64464bc226d160cd49f02fd23e09ec7b4
Arter Tendean <arter.tendean.07@gmail.com>
2024-01-16 18:37:10 +0800

    Adding tests

--- transactions/tests.py
+++ transactions/tests.py
@@ -1,3 +1,71 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from warehouses.models import Product, Supplier, Warehouse
+
+from .models import (
+    IncomingTransactionDetail,
+    IncomingTransactionHeader,
+    OutgoingTransactionDetail,
+    OutgoingTransactionHeader,
+)
+
+
+class TransactionModelTestCase(TestCase):
+    def setUp(self):
+        self.warehouse = Warehouse.objects.create(name="Test Warehouse")
+        self.supplier = Supplier.objects.create(name="Test Supplier")
+        self.product = Product.objects.create(name="Test Product")
+
+    def test_incoming_transaction_header_str_method(self):
+        incoming_header = IncomingTransactionHeader.objects.create(
+            warehouse=self.warehouse, supplier=self.supplier, notes="Test Notes"
+        )
+        self.assertEqual(str(incoming_header), str(incoming_header.transaction_number))
+
+    def test_incoming_transaction_detail_str_method(self):
+        incoming_header = IncomingTransactionHeader.objects.create(
+            warehouse=self.warehouse, supplier=self.supplier, notes="Test Notes"
+        )
+        incoming_detail = IncomingTransactionDetail.objects.create(
+            header=incoming_header,
+            product=self.product,
+            box_quantity=10,
+            pcs_quantity=5,
+        )
+        expected_str = str(incoming_header.transaction_number)
+        self.assertEqual(str(incoming_detail), expected_str)
+
+    def test_outgoing_transaction_header_str_method(self):
+        outgoing_header = OutgoingTransactionHeader.objects.create(
+            warehouse=self.warehouse, supplier=self.supplier, notes="Test Notes"
+        )
+        self.assertEqual(str(outgoing_header), str(outgoing_header.transaction_number))
+
+    def test_outgoing_transaction_detail_str_method(self):
+        outgoing_header = OutgoingTransactionHeader.objects.create(
+            warehouse=self.warehouse, supplier=self.supplier, notes="Test Notes"
+        )
+        outgoing_detail = OutgoingTransactionDetail.objects.create(
+            header=outgoing_header,
+            product=self.product,
+            box_quantity=10,
+            pcs_quantity=5,
+        )
+        expected_str = str(outgoing_header.transaction_number)
+        self.assertEqual(str(outgoing_detail), expected_str)
+
+    def test_transaction_header_created_at_auto_now_add(self):
+        incoming_header = IncomingTransactionHeader.objects.create(
+            warehouse=self.warehouse, supplier=self.supplier, notes="Test Notes"
+        )
+        outgoing_header = OutgoingTransactionHeader.objects.create(
+            warehouse=self.warehouse, supplier=self.supplier, notes="Test Notes"
+        )
+
+        self.assertTrue(
+            timezone.now() - incoming_header.datetime < timezone.timedelta(seconds=1)
+        )
+        self.assertTrue(
+            timezone.now() - outgoing_header.datetime < timezone.timedelta(seconds=1)
+        )
--- warehouses/tests.py
+++ warehouses/tests.py
@@ -1,3 +1,63 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Customer, Product, Supplier, Warehouse
+
+
+class ModelTestCase(TestCase):
+    def setUp(self):
+        self.supplier = Supplier.objects.create(name="Supplier 1")
+        self.customer = Customer.objects.create(name="Customer 1")
+        self.product = Product.objects.create(name="Product 1")
+        self.warehouse = Warehouse.objects.create(name="Warehouse 1")
+
+    def test_supplier_str_method(self):
+        self.assertEqual(str(self.supplier), "Supplier 1")
+
+    def test_customer_str_method(self):
+        self.assertEqual(str(self.customer), "Customer 1")
+
+    def test_product_str_method(self):
+        self.assertEqual(str(self.product), "Product 1")
+
+    def test_warehouse_str_method(self):
+        self.assertEqual(str(self.warehouse), "Warehouse 1")
+
+    def test_models_created_at_auto_now_add(self):
+        self.assertTrue(
+            timezone.now() - self.supplier.created_at < timezone.timedelta(seconds=1)
+        )
+        self.assertTrue(
+            timezone.now() - self.customer.created_at < timezone.timedelta(seconds=1)
+        )
+        self.assertTrue(
+            timezone.now() - self.product.created_at < timezone.timedelta(seconds=1)
+        )
+        self.assertTrue(
+            timezone.now() - self.warehouse.created_at < timezone.timedelta(seconds=1)
+        )
+
+    def test_models_updated_at_auto_now(self):
+        self.supplier.name = "Updated Supplier"
+        self.supplier.save()
+        self.assertTrue(
+            timezone.now() - self.supplier.updated_at < timezone.timedelta(seconds=1)
+        )
+
+        self.customer.name = "Updated Customer"
+        self.customer.save()
+        self.assertTrue(
+            timezone.now() - self.customer.updated_at < timezone.timedelta(seconds=1)
+        )
+
+        self.product.name = "Updated Product"
+        self.product.save()
+        self.assertTrue(
+            timezone.now() - self.product.updated_at < timezone.timedelta(seconds=1)
+        )
+
+        self.warehouse.name = "Updated Warehouse"
+        self.warehouse.save()
+        self.assertTrue(
+            timezone.now() - self.warehouse.updated_at < timezone.timedelta(seconds=1)
+        )
