c58e274efe3a5f950c8b8f2060b48a3851641d9f
Arter Tendean <arter.tendean.07@gmail.com>
2024-01-16 19:36:35 +0800

    Update warehouse view

--- warehouses/views.py
+++ warehouses/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from .models import Customer
+from .models import Customer, Product, Supplier, Warehouse
 
 
 def index(request):
@@ -11,4 +11,22 @@ def index(request):
 class CustomerListView(ListView):
     model = Customer
     template_name = "warehouses/customer-list.html"
+    ordering = ["-id"]
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "warehouses/product-list.html"
+    ordering = ["-id"]
+
+
+class SupplierListView(ListView):
+    model = Supplier
+    template_name = "warehouses/supplier-list.html"
+    ordering = ["-id"]
+
+
+class WarehouseListView(ListView):
+    model = Warehouse
+    template_name = "warehouses/warehouse-list.html"
     ordering = ["-id"]
